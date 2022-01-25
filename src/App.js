61fb310b5f9fcdc29c4f120dd6e10db651ad890d
aisha2000-hub <aisha.noori2000@gmail.com>
2022-01-25 22:54:38 +0100

    Die lösung von Aisha

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
 import "./App.css";
+import {useState} from "react";
 
 function App() {
+  const style = { backgroundColor: "green" ,
+   borderRadius: "10%",
+   margin: "10px",
+   padding:"5px",
+   color:"white",}
+  const [number, setNumber] = useState("");
+  const [secretNumber] = useState(Math.floor(Math.random() * 10) + 1);
+  const [message, setMessage] = useState("Please inter a number!");
+  const [counter, setCounter] = useState(0);
+
   return (
-    <div className="App">
-      <h1>Rate meine Zahl</h1>
+     <div className = "App" >
+   <h1>Guess a number from 1 to 10!</h1>
+
+    <input
+    value = {number}
+    onChange = {(event) => {
+        setNumber(event.target.value);
+      }}
+    />
+    <button  style={style}
+     onClick = {() => {
+        setCounter(counter + 1);
+        if (parseInt(number) === secretNumber) {
+          setMessage("Right!");
+        } else{
+           setMessage("Unfortunately it is wrong!");
+        }
+        if(counter === 10) {
+          setMessage("Start over!");
+          setCounter(0);
+        }
+      }} 
+      >
+    Guess 
+    </button> 
+     <div>
+      <p>Try: {counter}</p>
+       </div> <div>
+      <p>{message}</p>
+     </div>
     </div>
   );
 }
